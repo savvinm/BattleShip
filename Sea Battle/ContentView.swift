a1a0fd9c4070bc9eadd431048e47db931b9c82d8
Savvin Maksim <40721470+savvinm@users.noreply.github.com>
2022-03-10 12:54:38 -0800

    Initial Commit

---
+++ Sea Battle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sea Battle
+//
+//  Created by Maksim Savvin on 10.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sea Battle/Sea_BattleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sea_BattleApp.swift
+//  Sea Battle
+//
+//  Created by Maksim Savvin on 10.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct Sea_BattleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
